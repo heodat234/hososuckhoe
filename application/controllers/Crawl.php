8545308a532f743c3756410500fc78ae302ea0bd
nakama5211 <nakama@gmail.com>
2018-01-15 14:30:07 +0700

    2-30

---
+++ application/controllers/Crawl.php
@@ -0,0 +1,88 @@
+<?php //defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Crawl extends CI_Controller {
+
+	public function __construct(){
+		parent::__construct();
+		$this->load->helper(array('url','security','text'));
+		$this->load->library(array('form_validation','session','upload'));
+		$this->load->model(array('Hoso_model','ChitietHoso_model','Muc_Chiso_model','Login_model'));
+
+		$adata['pageHoso']		= 'active';
+		$this->_data['html_header'] 	= $this->load->view('hoso/header', $adata, TRUE);  
+        $this->_data['html_footer'] 	= $this->load->view('hoso/footer', NULL, TRUE);
+
+        $csrf = array(
+        'name' => $this->security->get_csrf_token_name(),
+        'hash' => $this->security->get_csrf_hash()
+		);
+		$this->a_Data['csrf'] = $csrf;
+	}
+
+	public function index(){
+		$this->data['crawl']		= 'active';
+		$this->_data['html_menu']		= $this->load->view('hoso/menu', $this->data, TRUE);  
+        $this->_data['html_body'] = $this->load->view('hoso/v_crawl', $this->a_Data, TRUE);
+        return $this->load->view('hoso/account',$this->_data);
+	}
+
+	public function crawl_result(){
+		$site_url = $this->input->post('url');
+		$keywords['id'] = 'mainContent';
+		$site_data = $this->get_site_data($site_url, 1, 0, $keywords);
+		$site_data['url'] = $site_url;
+		// Load view header       
+		$this->_data['html_header'] = $this->load->view('admin/header', $this->menu, TRUE);        
+		// Load  footer        
+		$this->_data['html_footer'] = $this->load->view('admin/footer', NULL, TRUE);      
+		// Load view method_one_view      
+		$this->_data['html_body'] = $this->load->view('admin/v_crawl_result', $site_data, TRUE);
+
+		$this->load->view('admin/master', $this->_data);
+
+		// print_r($site_data['content']);
+	}
+
+	function save_crawl(){
+		$frm = $this->input->post();
+		$record['category'] = '2';
+		$record['title'] = $frm['title'];
+		$record['image'] = $frm['image'];
+		$record['description'] = $frm['description'];
+		$record['content'] = $frm['content'];
+		if($this->M_table->add_data_row('project',$record)){
+			$data['success'] = "success";
+		}else{
+			$data['error'] = "error";
+		}
+		echo json_encode($data);
+	}
+
+	private function get_site_data($site_url, $max_depth = 1, $current_depth = 0, $keywords){
+		$current_depth++;
+		$this->load->library(array('crawler'));
+		$site_data = array();
+
+		if($this->crawler->set_url($site_url) !== false){
+			// $site_data['html'] = $this->crawler->get_html();
+			$site_data['title'] = $this->crawler->get_title();
+			$site_data['description'] = $this->crawler->get_description();
+			// $site_data['keywords'] = $this->crawler->get_keywords();
+			// $site_data['text'] = $this->crawler->get_text();
+			$site_data['image'] = $this->crawler->get_images();
+			$site_data['content'] = $this->crawler->get_contents_by_id($keywords['id']);
+			$site_data['links'] = $this->crawler->get_links();
+
+			if($current_depth <= $max_depth){
+				foreach($site_data['links'] as $link_key => &$link){
+					$link['data'] = $this->get_site_data($link, $max_depth, $current_depth);
+				}
+			}
+
+			return $site_data;
+		}
+		else{
+			return false;
+		}
+	}
+}
---
+++ application/views/hoso/v_crawl.php
@@ -0,0 +1,55 @@
+
+<div class="mednow-appointment-route-content">
+    <div class="modal-body" style="margin-top: 20px;margin-left: -55px; margin-right: 15px">
+        <table id="myTable" class=" table order-list">
+		    <thead>
+		        <tr>
+		            <td>Crawl</td>
+		        </tr>
+		    </thead>
+		    <tbody>
+		        <tr>
+		            <td class="col-sm-3">
+		                 <div class="input-group">
+		      <span class="input-group-addon"><i class="glyphicon glyphicon-tag"></i></span>
+		      <input id="product.name" type="text" class="form-control" name="product.name" placeholder="Tên nhà thuốc">
+		    </div>
+		            </td>
+		            <td class="col-sm-4">
+		                <div class="input-group">
+		      <span class="input-group-addon"><i class="	glyphicon glyphicon-link"></i></span>
+		      <input id="product.link" type="text" class="form-control" name="product.link" placeholder="Danh mục">
+		    </div>
+		            </td>
+		            <td class="col-sm-5">
+		                <div class="input-group">
+		      <span class="input-group-addon"><i class="glyphicon glyphicon-plus"></i></span>
+		      <input id="product.quantity" type="text" class="form-control" name="product.quantity" placeholder="Chi tiết danh mục">
+		    </div>
+		    <!-- <td class="col-sm-2">
+		            <div class="input-group">
+		                <label class="input-group-btn">
+		                    <span class="btn btn-primary">
+		                        Upload hình ảnh<input type="file" style="display: none;" multiple>
+		                    </span>
+		                </label>
+		                <input type="text" class="form-control" readonly>
+		            </div>
+		            </td>
+		            <td class="col-sm-2"><a class="deleteRow"></a>
+		            </td>
+		        </tr> -->
+		    </tbody>
+		    <tfoot>
+		        <tr>
+		            <td colspan="5" style="text-align: left;">
+		                <input type="button" class="btn btn-lg btn-block " id="addrow" value="Thêm sản phẩm" />
+		            </td>
+		        </tr>
+		        <tr>
+		        </tr>
+		    </tfoot>
+		</table>
+      </div>
+	</div>
+</div>
