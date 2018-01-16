777150a1cb6d0b9ff05ff523d738e91db1875966
nhuhuynhphamnguyen <35440725+nhuhuynhphamnguyen@users.noreply.github.com>
2018-01-16 14:28:36 +0700

    h

--- application/views/page/pageSearch.php
+++ application/views/page/pageSearch.php
@@ -29,129 +29,154 @@
     <div id="shortcode-12" style="margin-top: 10px;">
       <div class="container ">
         <div class="col-md-8 col-sm-12 col-lg-8 col-xs-12 left body-ds-tin"  >
-          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 " style="padding-bottom: 15px; ">
+          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 " style="padding-bottom: 28px;padding-top: 15px;border-bottom: 1px solid #f3f3f3">
             <div class="subtitle  pull-left news-sub" style="width: 100%">
               <div class="title-c">TÊN THUỐC</div>
               <div class="title-e"><p></p></div>
             </div>
             <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
+              <div class="icon-box-5 wow fadeInDown" data-wow-delay="0.5s" data-wow-offset="150">
+                <!-- <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div> -->
+                <div style="padding: 15px">
+                  <img src="<?php echo base_url() ?>images/thuoc.jpg" style ="width: 100%; height: 114px">
+                </div>
+                
+                <div class="icon-box2-title" style="margin-top: -2px">Điều trị bệnh tim</div>
                 <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
                 <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
               </div>   
             </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
+             <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
+              <div class="icon-box-5 wow fadeInDown" data-wow-delay="0.5s" data-wow-offset="150">
+                <!-- <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div> -->
+                <div style="padding: 15px">
+                  <img src="<?php echo base_url() ?>images/thuoc.jpg" style ="width: 100%; height: 114px">
+                </div>
+                
+                <div class="icon-box2-title" style="margin-top: -2px">Điều trị bệnh tim</div>
                 <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
                 <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
               </div>   
             </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
+             <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
+              <div class="icon-box-5 wow fadeInDown" data-wow-delay="0.5s" data-wow-offset="150">
+                <!-- <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div> -->
+                <div style="padding: 15px">
+                  <img src="<?php echo base_url() ?>images/thuoc.jpg" style ="width: 100%; height: 114px">
+                </div>
+                
+                <div class="icon-box2-title" style="margin-top: -2px">Điều trị bệnh tim</div>
+                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.sbchcbhscbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb</p>
                 <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
               </div>   
             </div>
           </div>
           
-          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 ">
-            <div class="subtitle  pull-left news-sub" style="width: 100%;margin-bottom: 15px;margin-top: 15px">
-              <div class="title-c">THEO BÁC SĨ</div>
-              <div class="title-e"><p></p></div>
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-          </div>
-
-
-          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 ">
-            <div class="subtitle  pull-left news-sub" style="width: 100%;margin-bottom: 15px;margin-top: 15px">
+          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 " style="padding-bottom: 28px;padding-top: 15px;border-bottom: 1px solid #f3f3f3">
+            <div class="subtitle  pull-left news-sub" style="width: 100%">
               <div class="title-c">THEO BỆNH VIỆN</div>
               <div class="title-e"><p></p></div>
             </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
+             <ul>
+              <li class="ds-tin" style="width: 100%; border-bottom: 1px dotted #ccc; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 218px">
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style=""  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjs
+                      </h5>
+                    </div>
+                    <div>
+                      <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
+                    </div>
+                  </div>
+              </li>
+               <li class="ds-tin-a" style="width: 100%; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 140px;box-sizing: border-box;" >
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style="margin-top: 15px; width: 280px; height: 150px; border-radius: 5px"  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrvsvcgvsvcgsdtgvcsdgxcvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                      </h5>
+                    </div>
+                    <div>
+                      <p>ascysycgtdsc</p>
+                    </div>
+                  </div>
+              </li>
+          </ul>
+          </div>
+
+
+         <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 " style="padding-bottom: 28px;padding-top: 15px;border-bottom: 1px solid #f3f3f3">
+            <div class="subtitle  pull-left news-sub" style="width: 100%">
+              <div class="title-c">TÊN THUỐC</div>
+              <div class="title-e"><p></p></div>
+
             </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
+            <ul>
+              <li class="ds-tin" style="width: 100%; border-bottom: 1px dotted #ccc; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 218px">
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style=""  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjs
+                      </h5>
+                    </div>
+                    <div>
+                      <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
+                    </div>
+                  </div>
+              </li>
+               <li class="ds-tin-a" style="width: 100%; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 140px;box-sizing: border-box;" >
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style="margin-top: 15px; width: 280px; height: 150px; border-radius: 5px"  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrvsvcgvsvcgsdtgvcsdgxcvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                      </h5>
+                    </div>
+                    <div>
+                      <p>ascysycgtdsc</p>
+                    </div>
+                  </div>
+              </li>
+          </ul>
           </div>
           
-          <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 ">
-            <div class="subtitle  pull-left news-sub" style="width: 100%;margin-bottom: 15px;margin-top: 15px">
-              <div class="title-c">THEO TIN TỨC</div>
+        <div class="col-md-12 col-sm-12 col-lg-12 col-xs-12 " style="padding-bottom: 28px;padding-top: 15px;border-bottom: 1px solid #f3f3f3">
+            <div class="subtitle  pull-left news-sub" style="width: 100%">
+              <div class="title-c">TIN TỨC</div>
               <div class="title-e"><p></p></div>
             </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
-            <div class="col-sm-6 col-xs-12 col-md-4  col-lg-4">
-              <div class="icon-box-3 wow fadeInDown" data-wow-delay="0.9s" data-wow-offset="150">
-                <div class="icon-boxwrap2"><i data-icon="\e609" class="icon-stethoscope icon-box-back2"></i></div>
-                <div class="icon-box2-title">Hỗ trợ điều trị</div>
-                <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
-                <!-- <div class="iconbox-readmore"><a href="#">Read More</a></div> -->
-              </div>   
-            </div>
+           <ul>
+              <li class="ds-tin" style="width: 100%; border-bottom: 1px dotted #ccc; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 218px">
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style=""  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjs
+                      </h5>
+                    </div>
+                    <div>
+                      <p>Phân tích chỉ số sức khỏe, biểu đồ kết quá sức khỏe trực quan hỗ trợ điều trị các bệnh mãn tính.</p>
+                    </div>
+                  </div>
+              </li>
+               <li class="ds-tin-a" style="width: 100%; padding-top: 0px;padding-right: 10px;padding-bottom: 10px;padding-left: 4px;height: 140px;box-sizing: border-box;" >
+                    <a href="<?php echo base_url().'tintuc/1'?>"><img style="margin-top: 15px; width: 280px; height: 150px; border-radius: 5px"  class="img-responsive" src="<?php echo base_url().'images/benhvien.jpg' ?>"></a>
+                  <div class="ds-tin-content" >
+                    <div><h3><a href="<?php echo base_url().'tintuc/'?>">jyhntbgvhttynyjhtbgrvsvcgvsvcgsdtgvcsdgxcvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv</a></h3></div>
+                    <div>
+                      <h5>ynbtbrbgbsjcnjskkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkkk
+                      </h5>
+                    </div>
+                    <div>
+                      <p>ascysycgtdsc</p>
+                    </div>
+                  </div>
+              </li>
+          </ul>
           </div>
           <div class="pagination-page"></div>
         </div>
